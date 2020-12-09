ce26fc52c389ea8e5028d6af41bf9f6b4cbe8e5d
BunchOfBytes <69111581+BunchOfBytes@users.noreply.github.com>
2020-12-09 16:27:58 +0800

    Update script.py

--- Cloud/script.py
+++ Cloud/script.py
@@ -1,1 +1,18 @@
+##Create a list of words from wordlist
+wordlist=[]
+file = 'wordlist.txt'
+with open(file, 'r') as f:
+    for line in f:
+        line = line.rstrip("\n")    
+        wordlist.append(line)
 
+##Print the words to be outputted which will be redirected into a file later
+for i in wordlist:
+   str=""
+   str+=(i+"-")
+   print(str)
+   for j in wordlist:
+       str+=(j+"-")
+       str+="s4fet3ch"
+       print(str)
+       str=(i+"-")
